0b7750ac4ccc54b3aa099a4e9b92dd78285bd0e8
quarterwire <watcher.alex0411@gmail.com>
2025-10-16 04:45:34 +0300

    added UI just to see output from endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,6 +7,11 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 from app.services.cache import download_file
 from app.controllers import game as game_controller
+from fastapi.middleware.cors import CORSMiddleware
+
+# CORS Policy
+origins = ["http://localhost:5173", "http://localhost:5173"]
+
 
 # Initialize the scheduler instance
 scheduler = AsyncIOScheduler(id="ye")
@@ -42,7 +47,13 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(game_controller.router)
 
 
